1282a985ac51ed840b2888d747f34b4649c21778
natasa042 <natasaugrenovic25@gmail.com>
2023-02-03 15:45:27 +0100

    cleanup

--- htlvideo/schemas/index.ts
+++ htlvideo/schemas/index.ts
@@ -1,10 +1,8 @@
-import blockContent from './blockContent'
 import text from './text'
 import post from './post'
 import picture from './picture'
-import slider from './slider'
 import carousel from './carousel'
 import gallery from './gallery'
 
 
-export const schemaTypes = [post, picture, text, carousel, gallery, slider, blockContent]
+export const schemaTypes = [post, picture, text, carousel, gallery]
--- video/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'g2u11ms5',
-    dataset: 'production'
-  }
-})
